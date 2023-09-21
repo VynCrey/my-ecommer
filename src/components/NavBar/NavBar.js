def69cb83a6d56390fdf318b581f9c1527b34e6e
VynCrey <vyncrey03@gmail.com>
2023-09-21 09:05:07 -0300

    Modificacion

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,8 +8,7 @@ const NavBar = () => {
   return (
     <nav>
         <ul className='ul-1'>
-            <li>about</li>
-            <li>icont</li>
+        <li><Button variant="contained">Click Aqui</Button></li>
         </ul>
         <ul className='ul-2'>
             <li>contact</li>
@@ -17,7 +16,7 @@ const NavBar = () => {
             <li>icont</li>
         </ul>
         <ul className='ul-3'>
-          <li><Button variant="contained">Click Aqui</Button></li>
+          
             <CardWidget/>
             
             
